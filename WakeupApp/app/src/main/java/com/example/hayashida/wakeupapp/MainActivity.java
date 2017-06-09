e7a5d4c7305961b549ef87a6cef5e70320854369
dashiya <hadashiya.duck@gmail.com>
2017-06-09 17:05:51 +0900

    First commit

--- WakeupApp/app/src/main/java/com/example/hayashida/wakeupapp/MainActivity.java
+++ WakeupApp/app/src/main/java/com/example/hayashida/wakeupapp/MainActivity.java
@@ -2,8 +2,21 @@ package com.example.hayashida.wakeupapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+    PendingIntent pendingIntent = getPendingIntent();
+if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(alarmTimeMillis, null), pendingIntent);
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, alarmTimeMillis, pendingIntent);
+    } else {
+        alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTimeMillis, pendingIntent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
